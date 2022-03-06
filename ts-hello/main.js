8efe3091ced4c1b036103156cc37be1ba938c7df
ali shirzadeh <ashirzade5@gmail.com>
2022-03-06 21:04:02 +0330

    create project

---
+++ ts-hello/main.js
@@ -0,0 +1,12 @@
+// function log(message) {
+//     console.log(message)
+// }
+// var message = 'Hello world'
+// log(message)
+function doSomeThing() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally:" + i);
+}
+doSomeThing();
---
+++ ts-hello/main.ts
@@ -0,0 +1,19 @@
+// function log(message) {
+//     console.log(message)
+// }
+
+// var message = 'Hello world'
+
+// log(message)
+
+function doSomeThing(){
+    for (let i = 0; i <5; i++) {
+      console.log(i)
+        
+    }
+
+    console.log("Finally:" + i)
+}
+
+doSomeThing()
+
